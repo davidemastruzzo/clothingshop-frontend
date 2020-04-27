5e7d3b2a89aa03e2b322868616f83eb0da0d4cce
jan_preisig <jan_preisig@gmx.net>
2020-04-27 10:45:07 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ItemDetailsComponent} from './components/item-details/item-details.component';
+import {ItemsOverviewComponent} from './components/items-overview/items-overview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "/item-details", component: ItemDetailsComponent},
+  {path: "/items-overview", component: ItemsOverviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
