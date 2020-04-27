5a10dda115ca93d17185f8afb16b6fc21b3339ce
Davide Mastruzzo <mastruzzodavide@gmail.com>
2020-04-27 12:04:13 +0200

    add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ItemDetailsComponent} from './components/item-details/item-details.component';
 import {ItemsOverviewComponent} from './components/items-overview/items-overview.component';
+import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
 
 
 const routes: Routes = [
   {path: '', component: ItemsOverviewComponent },
   {path: 'item-details', component: ItemDetailsComponent},
-  {path: 'items-overview', component: ItemsOverviewComponent}
+  {path: 'items-overview', component: ItemsOverviewComponent},
+  {path: 'cart', component: ShoppingCartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ItemDetailsComponent } from './components/item-details/item-details.com
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
